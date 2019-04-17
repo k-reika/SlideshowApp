76fe4bf45500aa5dd67baeb07cc812f78c7f6624
菊池玲花 <r-kikuchi@i-enter.co.jp>
2019-04-17 15:44:50 +0900

    Initial Commit

---
+++ SlideshowApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp2
+//
+//  Created by 菊池 玲花 on 2019/04/17.
+//  Copyright © 2019 reika.kikuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
